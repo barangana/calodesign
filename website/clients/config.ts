e8f2be48943bf1dd2c3dae001d814b46f2abee7f
Adriel <baranganadriel@gmail.com>
2022-04-20 23:36:59 -0400

    feat: configure sanity

---
+++ website/clients/config.ts
@@ -0,0 +1,14 @@
+export interface Config {
+  sanity: {
+    projectId: string
+    dataset: string
+    // token: string
+  }
+}
+
+export const config: Config = {
+  sanity: {
+    projectId: process.env.NEXT_SANITY_PROJECT_ID || '',
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  },
+}
---
+++ website/pages/projects/[slug].tsx
@@ -0,0 +1,11 @@
+import { GetStaticProps, NextPage } from 'next'
+
+const Project: NextPage = () => {
+  return <div>I am a single project</div>
+}
+
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   // ...
+// }
+
+export default Project
