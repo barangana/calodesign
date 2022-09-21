68d497fcba713223b7e4036bef31edcf8d812fad
Adriel <baranganadriel@gmail.com>
2022-09-20 23:35:59 -0400

    feat(website): footer done

--- website/components/ui/Footer.tsx
+++ website/components/ui/Footer.tsx
@@ -1,5 +1,15 @@
+import { Flex, Link, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
+import info from '../../utils/info.json'
+import NextLink from 'next/link'
 
-export const Footer = () => {
-  return <div>Copyrights</div>
+export const Footer: React.FC = () => {
+  return (
+    <Flex direction='column' p={4}>
+      <Text align='center'>
+        {`${info.copyright} ${new Date().getFullYear()} — `}
+        <NextLink href='https://github.com/barangana'>{info.branding}</NextLink>
+      </Text>
+    </Flex>
+  )
 }
