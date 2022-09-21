3be74b573d25fe7744fc3cb6f5c5ee5eba5ba8a3
Adriel <baranganadriel@gmail.com>
2022-09-20 23:18:00 -0400

    feat: header done

--- website/components/ui/Header.tsx
+++ website/components/ui/Header.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import { Button, Flex, Heading, Link, Stack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import {
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  IconButton,
+} from '@chakra-ui/react'
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
 import NextLink from 'next/link'
 import info from '../../utils/info.json'
 
-// TODO: Might have to change the title to logo : JSX.Element
+// TODO: Might have to change the title to logo : JSX.Element. Come back and fix the IconButtons.
 interface HeaderProps {
-  title?: string
+  title: string
 }
 
 type LinksType = {
@@ -20,22 +28,75 @@ const Links: LinksType[] = [
 ]
 
 export const Header: React.FC<HeaderProps> = ({ title }) => {
+  const [display, setDisplay] = useState('none')
   return (
-    <Flex p={4} justify='space-between'>
+    <Flex p={2} justify='space-between'>
       <Stack direction={['column', 'row']} align='center'>
         <NextLink href='/' passHref>
           <Link>
-            <Heading size='lg'>{title}</Heading>
+            <Heading size='lg'>{title.toUpperCase()}</Heading>
           </Link>
         </NextLink>
         {Links.map((link) => (
           <NextLink key={link.label} href={link.href} passHref>
-            <Link p={4}>{link.label}</Link>
+            <Link p={4} display={['none', 'none', 'flex', 'flex']}>
+              {link.label}
+            </Link>
           </NextLink>
         ))}
       </Stack>
-      <Flex align='center'>
+      <Flex align='center' display={['none', 'none', 'flex', 'flex']}>
         <Button variant='outline'>{info.request_button}</Button>
+      </Flex>
+      <IconButton
+        aria-label='Open Menu'
+        size='sm'
+        icon={<HamburgerIcon />}
+        display={['flex', 'flex', 'none', 'none']}
+        onClick={() => setDisplay('flex')}
+      />
+
+      <Flex
+        w='100vw'
+        bg='white'
+        zIndex={20}
+        h='100vh'
+        pos='fixed'
+        top='0'
+        left='0'
+        overflowY='auto'
+        direction='column'
+        display={display}
+        justify='center'
+      >
+        {/* <Flex justify='flex-end'>
+          <IconButton
+            mt={2}
+            mr={6}
+            aria-label='Close Menu'
+            size='sm'
+            icon={<CloseIcon />}
+            onClick={() => setDisplay('none')}
+          />
+        </Flex> */}
+
+        <Flex direction='column' align='center'>
+          <Stack align='center'>
+            <NextLink href='/' passHref>
+              <Link>
+                <Heading size='lg'>Home</Heading>
+              </Link>
+            </NextLink>
+            {Links.map((link) => (
+              <NextLink key={link.label} href={link.href} passHref>
+                <Link p={4}>{link.label}</Link>
+              </NextLink>
+            ))}
+          </Stack>
+          <Flex align='center'>
+            <Button variant='outline'>{info.request_button}</Button>
+          </Flex>
+        </Flex>
       </Flex>
     </Flex>
   )
