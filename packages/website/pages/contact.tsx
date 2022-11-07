1f891790f8c5f30bf9eb3e37b4b63ca4470c93cf
Adriel <baranganadriel@gmail.com>
2022-11-07 16:02:38 -0500

    fix: small bug

--- packages/website/pages/contact.tsx
+++ packages/website/pages/contact.tsx
@@ -1,12 +1,11 @@
 import { NextPage } from 'next'
 import React from 'react'
 import { Contacts, Layout } from '../components'
-import info from '../utils/info.json'
 
 export const ContactPage: NextPage = () => {
   return (
     <Layout>
-      <Contacts contacts={info.contacts} height='100vh' />
+      <Contacts />
     </Layout>
   )
 }
